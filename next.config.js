014cf07411a8109f054f317bb6823b9126190c14
Lucky <lucky123@gmail.com>
2024-10-10 02:14:51 +0530

    868

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "lh3.googleusercontent.com",
-        },
-      ],
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    reactStrictMode: false,
+    
   };
   
   export default nextConfig;
