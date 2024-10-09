dada867aaabdf84f3047fb9bae0b40dba3b7b3d5
Lucky <lucky123@gmail.com>
2024-10-10 01:03:30 +0530

    account up

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -2,8 +2,14 @@
 
 import React from 'react'
 import { Mail } from './mail';
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs/server';
 
 export default function MailDashboard() {
+  const { userId } = auth()
+  if (userId) {
+      return redirect('/mail')
+  }
     return (
       <Mail  
       
